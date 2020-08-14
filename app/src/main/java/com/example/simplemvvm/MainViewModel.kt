ea33933ff4e160e0a51b2bad20be1c2b6860b6ac
Joe Timmins <joe@tickr.co.uk>
2020-08-14 16:27:42 +0100

    Add skeleton view model

---
+++ app/src/main/java/com/example/simplemvvm/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.simplemvvm
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModel(
+    private val initialState: MainUiState
+) : ViewModel() {
+    private val _uiState = MutableLiveData(initialState)
+    val uiState = _uiState as LiveData<MainUiState>
+
+}
+
+class MainViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val initialState = MainUiState(89)
+        val viewModel = MainViewModel(initialState)
+        return modelClass.cast(viewModel)!!
+    }
+}
+
+data class MainUiState(
+    val total: Int
+)
